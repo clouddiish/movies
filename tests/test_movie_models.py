22ad281fd7bed3199b5f4b4b888c84b16c77b593
Olga Szymczak <140915825+clouddiish@users.noreply.github.com>
2025-01-21 22:06:38 +0100

    test_movie_models.py start

--- tests/test_movie_models.py
+++ tests/test_movie_models.py
@@ -1,1 +1,17 @@
 import pytest
+from pydantic import ValidationError
+from ex6.models.movie_models import MovieBase, MovieOut, MovieIn
+
+
+def test_movie_base_valid():
+    movie = MovieBase(
+        title="Test Movie",
+        director="Test Director",
+        category="test category",
+        year=2010,
+    )
+
+    assert movie.title == "Test Movie"
+    assert movie.director == "Test Director"
+    assert movie.category == "test category"
+    assert movie.year == 2010
